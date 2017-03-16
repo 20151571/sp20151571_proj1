b259f77a29ee7199356f21cb29a982529673efb0
20151571 <dbdydgur2244@gmail.com>
2017-03-16 11:11:30 -0700

    command add

--- 20151571.h
+++ 20151571.h
@@ -45,33 +45,33 @@ typedef struct MEMORY{
     int max_address;
 }Shell_Memory;
 
-int min(int a, int b);
-void str_replace(char *, const char *, const char *);
-int get_values(char *);
-int command_find(char *str_cmp);
-int get_command(char *buffer);
-void add_history(char *command);
+int min(int a, int b); //a,b 값중작은 값을 반환하는 함수
+void str_replace(char *, const char *, const char *); //문자열 치환 함수
+int get_values(char *); // 명령어에서 값들을 가져오는 함수
+int command_find(char *str_cmp); // 입력한 명령어가 있는지 찾고몇번째 명령어 인지  찾는 함수
+int get_command(char *buffer); // 입력한 명령어를 사용하기 위해 적절히 숫자를 지정해주는 함수
+void add_history(char *command); // history에 추가하는 함수
 
-void init();
+void init(); // 초기화
 
-int Hash_find(char *);
+int Hash_find(char *); // hash에서 찾는 함수
 
-void Hash_insert(int , char *mnemonic);
-void Link_insert();
+void Hash_insert(int value, char *mnemonic); // hash에 insert하는 함수
+void Link_insert(); // linked_list insert 함수
 
-void print_help();
-void print_dir();
-void print_history();
-void print_opcodelist();
-int print_memory(int , int );
+void print_help(); // helplist를 출력하는 함수
+void print_dir(); // dir를 출력하는 함수
+void print_history(); // history를 출력하는 함수
+void print_opcodelist(); // opcodelist를 출력하는 함수
+int print_memory(int start, int end); // memory를 start부터 end까지 출력하는 함수
 
-void command_dump();
-int command_edit(char *buffer);
-int command_fill(char *buffer);
-void command_reset();
-void command_opcode();
-int command_check(char *buffer);
-void process_quit();
-void main_process(char *buffer);
+void command_dump(); // 입력한 명령어가 dump인 경우 처리해주는 함수
+int command_edit(char *buffer); // 입력한 명령어가 edit인 경우 처리해주는 함수
+int command_fill(char *buffer); // fill인 경우 처리해주는 함수
+void command_reset(); // reset인 경우 처리해주는 함수
+void command_opcode(); // opcode인 경우 처리해주는 함수
+int command_check(char *buffer); // 명령어를 옳바르게 입력한지 확인해주는 함수력
+void process_quit(); // 입력한 명령어가 quit 일 때 process를 종료시켜주는 함수
+void main_process(char *buffer); // 명령어 string을 받아처리해주는 함수
 
 #endif
--- parkunsang.c
+++ parkunsang.c
@@ -33,59 +33,67 @@ const char *Help[] = {
     "opcodelist"
 };
 
-int values[5];
-char *instruction[10];
-char str_copy[256];
-Hash hash_table;
-History Hhead = NULL;
-Lnode Lhead = NULL;
-Shell_Memory Sh_memory;
+int values[5]; // 입력한 명령어에서 value들을 저장하는 배열(address, start, end 등등)
+char *instruction[10]; // 명령어를 space나 tab을 기준으로 tokenize 했을 때나오는 string을 저장하는 배열
+char str_copy[256]; // string copy용 배열
+Hash hash_table; // hash_table 구조체
+History Hhead = NULL; // history를 linked_list로 구현한 head
+Lnode Lhead = NULL; // linked_list head
+Shell_Memory Sh_memory; // 메모리 정보를 담고 있는 구조체
 
+// a ,b  값중 작은 값을 반환해준다.
 int min(int a, int b){
     return a < b ? a : b;
 }
 
+/* target string에서 orig과 같은 부분을 repl으로 replace 시켜주는 함수이다.
+ * char *target이바꿀 string이고 orig은replace될 string, repl은 orig을 replace할 string이다.
+ */
 void str_replace(char *target, const char *orig, const char *repl){
-    char buffer[256] = { 0 };
-    char *insert_point = &buffer[0];
-    const char *tmp = target;
-    size_t orig_len = strlen(orig);
+    char buffer[256] = { 0 }; // target string을 copy할 배열
+    char *insert_point = buffer; // replace할 point를 저장할 pointer
+    const char *tmp = target; // target의원래 주소를 저장한 pointer
+    size_t orig_len = strlen(orig); 
     size_t repl_len = strlen(repl);
     while (1) {
-        const char *p = strstr(tmp, orig);
-        if (p == NULL) {
-            strcpy(insert_point, tmp);
+        const char *p = strstr(tmp, orig); // p = orig을 포함한 문자열의 위치
+        if (p == NULL) { // 더이상 orig을 포함한 문자열이 존재하지 않음
+            strcpy(insert_point, tmp); // insert_point에 tmp string copy
             break;
         }
         memcpy(insert_point, tmp, p - tmp);
         insert_point += p - tmp;
         memcpy(insert_point, repl, repl_len);
         insert_point += repl_len;
-        tmp = p + orig_len;
+        tmp = p + orig_len; // insert_point를 찾고 buffer에 저장하는 부분
     }
-    strcpy(target, buffer);
+    strcpy(target, buffer); //targer에 replacement한string을복사함
 }
 
+/* 입력한 명령어에서 값들을 저장하는 함수
+ * 값들이 정상적인 경우 return 1
+ * 이상한 입력이 들어온 경우 return -1
+ */
 int get_values(char *buffer){
     char *token;
     char *error;
     char sep[] = " \t";
-    int idx= 0, flag = 1;
+    int idx= 0, flag = 1; // flag는 에러 체크할 변수
     int value;
     token = strtok(buffer, sep);
     while(token != NULL){
         token = strtok(NULL, sep);
         if ( token == NULL)
             break;
-        if ( strcmp( token , "," ) == 0 ){
+        if ( strcmp( token , "," ) == 0 ){ // ',' 에러 처리
             if ( flag )
                 return -1;
             else
                 flag = 1;
         }
         else{
-            value = (int)strtol(token, &error, 16);
-            if( *error != '\0')
+            value = (int)strtol(token, &error, 16); // 16진수의 값을 10진수로 바꿈
+            if( *error != '\0') // 16진수가 아닌 경우 에러 처리
                 return -1;
             values[idx++] = value;
             flag = 0;
@@ -94,18 +102,24 @@ int get_values(char *buffer){
     return 1;
 }
 
+/* 입력한 명령어가존재하는지 찾고 몇번째와 일치하는지 알려주는 함수
+ * 존재하는 경우 return 번호
+ * 존재하지 않는 경우 return -1
+ */
 int command_find(char *str_cmp){
     int i;
     int size = sizeof(Help) / sizeof(char *);
     printf("command is %s\n", str_cmp);
-    for ( i = 0; i < size; ++i){
+    for ( i = 0; i < size; ++i)
         if ( strcmp(Help[i], str_cmp) == 0)
             return i;
-    }
-    printf("there is no comamnd\n");
+    printf("The corresponding command does not exist.\n"); 
     return -1;
 }
 
+/* 입력한 명령어가 존재하면 enum command에 있는 값으로 적절히 값을return하는 함수
+ * 명령어가 없는 경우만 return -1을 한다.
+ */
 int get_command(char *buffer){
     char sep[] = " \t";
     char *token = strtok(buffer, sep);
@@ -113,11 +127,13 @@ int get_command(char *buffer){
     while(token != NULL){
         if ( ( command_num = command_find(token) ) != -1 )
             return ( command_num <= 11 ? command_num / 2 : command_num - 5);
+        // dir와 같은 명령어들은 d[ir]이런 형식으로 사용할 수 있어서 이 부분을 처리해주는 부분이다.
         token = strtok(NULL, sep);
     }
     return -1;
 }
 
+// opcode.txt에서 opcode를입력받아서 저장하는 함수
 void get_opcode(){
     FILE *fp;
     char buffer[256];
@@ -132,15 +148,14 @@ void get_opcode(){
     }
 
     while ( fgets(buffer, sizeof(buffer), fp) != NULL ){
-        sscanf(buffer, "%x %s %s", &n_opcode, str_opcode, code );
-        Hash_insert(n_opcode, str_opcode);
+        sscanf(buffer, "%x %s %s", &n_opcode, str_opcode, code ); //space나 tab 같은 것들 처리
+        Hash_insert(n_opcode, str_opcode); //hash에 insert
     }
-
     fclose(fp);
 }
 
 
-
+// history에 입력한 명령어를 linked_list tail에  추가하는 함수
 void add_histroy(char *command){
     History ptr = Hhead;
     History nptr;
@@ -150,40 +165,47 @@ void add_histroy(char *command){
     strncpy(nptr->command, command, sizeof(nptr->command));
 
     if(ptr != NULL){
-        for(; ptr-> next != NULL; ptr = ptr -> next);
+        for(; ptr-> next != NULL; ptr = ptr -> next); // 가장 최근에 입력한 명령어를 가장 나중에 넣는다.
         ptr -> next = nptr;
     }
     else
         Hhead = nptr;
 }
 
+// 초기화 함수
 void init(){
     int i;
     hash_table.size = 20;
-    for ( i = 0; i < hash_table.size; ++i ) {
+    for ( i = 0; i < hash_table.size; ++i )
         hash_table.Table[i] = NULL;
-    }
     
     Sh_memory.last_address = 1048575;
     Sh_memory.max_address = 1048575;
     for( i = 0; i < Sh_memory.max_address; ++i)
         Sh_memory.memory[i] = 0;
-    get_opcode();
+    get_opcode(); //opcode 저장
 }
 
+/* mnemonic string에 해당하는 opcode가 존재하는지 hash에서 찾는 함수
+ * 존재하면 number opcode를 return
+ * 존재하지 않으면 return -1
+ */
 int Hash_find(char *mnemonic){
     int opcode = -1;
     int i;
     Hnode ptr;
-    for ( i = 0; i < hash_table.size; ++i ){
+    for ( i = 0; i < hash_table.size; ++i ){ // hash_table에서 일일이 찾는다
         for ( ptr = hash_table.Table[i]; ptr != NULL; ptr = ptr -> next ){
             if ( strcmp( ptr->str_opcode , mnemonic ) == 0)
                 return  ( opcode = ptr -> n_opcode );
         }
     }
-    return opcode;
+    return -1; // 존재하지 않는 경우
 }
 
+/* Hash에 mnemonic을 insert하는 함수
+ * 적절한 key를 사용하여 insert한다(여기서는 key % hash_size를 이용한다)
+ */
 void Hash_insert(int key, char *mnemonic){
     Hnode ptr;
     Hnode nptr;
@@ -197,7 +219,7 @@ void Hash_insert(int key, char *mnemonic){
     key = key % Hash_size;
     ptr = hash_table.Table[key];
 
-    if(ptr != NULL){
+    if(ptr != NULL){ // hash_table이 빈 경우
         nptr -> next = ptr;
         hash_table.Table[key] = nptr;
     }
@@ -206,6 +228,7 @@ void Hash_insert(int key, char *mnemonic){
         hash_table.Table[key] = nptr;
 }
 
+// linked_list insert 함수
 void Link_insert(){
     Lnode nptr;
     Lnode ptr;
@@ -223,31 +246,33 @@ void Link_insert(){
         Lhead = nptr;
 }
 
+//가능한 명령어를 출력하는 함수
 void print_help(){
     int i;
     for(i = 0; i < 9; ++i)
         printf("%s\n", help_list[i]);
 }
 
+// dir를 출력하는 함수
 void print_dir(){
     DIR *dirp;
     struct dirent *direntp;
     struct stat buf;
-    if( (dirp = opendir(".")) == NULL){
+    if( (dirp = opendir(".")) == NULL){ //diropen error check
         printf("Can not Open Directory\n");
         return;
     }
 
-    while( (direntp = readdir(dirp)) != NULL){
+    while( (direntp = readdir(dirp)) != NULL){ // dir에 있는 모든 파일을 읽을 때 까지
         stat(direntp->d_name, &buf);
         printf("%s", direntp->d_name);
 
-        if( S_ISDIR(buf.st_mode) )
+        if( S_ISDIR(buf.st_mode) ) //directory인 경우
             printf("/");
 
         else if( (buf.st_mode & S_IXUSR) ||
                 (buf.st_mode & S_IXGRP) ||
-                (buf.st_mode & S_IXOTH) )
+                (buf.st_mode & S_IXOTH) ) // 실행 파일인 경우
             printf("*");
         printf("\t");
     }
@@ -255,7 +280,7 @@ void print_dir(){
     closedir(dirp);
 }
 
-
+//history 목록을 출력하는 함수
 void print_history(){
     History ptr;
     int i = 1;
@@ -263,6 +288,7 @@ void print_history(){
         printf("%-5d %s\n", i++, ptr->command);
 }
 
+//opcodelist를 출력하는 함수
 void print_opcodelist(){
     Hnode ptr;
     for ( int i = 0; i < 20; ++i){
@@ -276,15 +302,19 @@ void print_opcodelist(){
     }
 }
 
+/* start부터 end까지 memory 정보를 출력하는 함수
+ * boundary check 에서 boundary error가 발견되면 return -1
+ * 정상적인 경우 return 1
+ */
 int print_memory(int start, int end){
     int str_hex = start / 16 * 16,
-        end_hex = end / 16 * 16;
+        end_hex = end / 16 * 16; // 16단위로 끊어서 세기 위함
     int i, j;
     char *memory = Sh_memory.memory;
     
-    if(start <= end && start >= 0){
-        end = end <= Sh_memory.max_address ? end : Sh_memory.max_address ;
-        Sh_memory.last_address = end;
+    if(start <= end && start >= 0){ // boundary check
+        end = end <= Sh_memory.max_address ? end : Sh_memory.max_address ; // end boundary check
+        Sh_memory.last_address = end; // last_address update
         for ( i = str_hex; i <= end_hex; i += 16){
             printf("%05X ", i);
             for(j = 0; j < 16; ++j){
@@ -308,23 +338,29 @@ int print_memory(int start, int end){
             }
             puts("");
         }
-        return 1;
+        return 1; // 정상인 경우
     }
-    else
+    else // boundary check에서 에러확인된경우
         return -1;
 }
 
-
+/* dump 명령어를 처리해주는 함수
+ * dump만 입력한 경우 last_address 다음 주소에서 160칸 출력(메모리 범위를 넘어가지 않는 범위에서)
+ * dump start만 입력한 경우 start에서 160칸 출력(단 메모리 범위를 넘어가지 않는 범위에서)
+ * dump start. end만 입력한 경우 start부터 end까지 출력해준다.
+ * start. end가 16진수가 아니거나 범위를 넘어가는 경우는 출력하지 않고 에러처리한다.
+ */
 void command_dump(){
     int len = 0;
     int start_address = ( Sh_memory.last_address + 1 ) % 
         ( Sh_memory.max_address + 1 ) ,
         end_address = min ( start_address + 159, Sh_memory.max_address ); 
+    // start_address와 end_address 기본 dump인 경우를 위한  초기화 부분.
     char *Error1 = '\0', *Error2 = '\0';
 
-    for ( int i = 0; instruction[i] != NULL; ++i, len++);
+    for ( int i = 0; instruction[i] != NULL; ++i, len++); // 명령어에 인자 개수 체크
     
-    if ( len == 2){
+    if ( len == 2){ // dump start인 경우 거나 dump start,end인 경우
         start_address = (int)strtol(instruction[1], &Error1, 16);
         if(*Error1 == ','){
             end_address = (int)strtol(Error1+1, &Error2, 16);
@@ -332,20 +368,23 @@ void command_dump(){
         }
         if ( start_address > Sh_memory.max_address ||
                start_address > end_address || *Error2 != '\0'){
+            // boundary check와 16진수가 입력된것인지 체크
             printf("Address Error!\nStart_address exceeds max_address\n");
             return;
         }
         end_address = min ( end_address, Sh_memory.last_address );
     }
 
-    else if ( len == 3){
+    else if ( len == 3){ // dump start, end와 dump start ,end인 경우
         if ( instruction[1][ strlen(instruction[1]) - 1] == ',' )
             instruction[1][ strlen(instruction[1]) -1 ] = '\0';
+        // dump start, end인 경우
 
         else if( instruction[2][0] == ',' ){
-            for ( int i = 0;  i < strlen(instruction[2]); ++i)
+            for ( int i = 0;  i < (int)strlen(instruction[2]); ++i)
                 instruction[2][i] = instruction[2][i+1];
         }
+        // dump start ,end 인 경우
 
         start_address = (int)strtol(instruction[1], &Error1, 16);
         end_address = (int)strtol(instruction[2], &Error2, 16);
@@ -353,24 +392,26 @@ void command_dump(){
 
         if ( *Error1 != '\0' || *Error2 != '\0' || start_address > end_address ||
                 start_address > Sh_memory.max_address ){
+            // boundary check와 16진수가 아닌 경우.
             printf("Address Error!\nStart_address exceeds max_address\n");
             return;
         }
     }
 
-    else if ( len == 4){
+    else if ( len == 4){ // dump start , end인 경우
         start_address = (int)strtol(instruction[1], &Error1, 16);
         end_address = (int)strtol(instruction[3], &Error2, 16);
         
         if ( *Error1 != '\0' || *Error2 != '\0' || 
                 strcmp(instruction[2], ",") != 0 || start_address > end_address ||
                 start_address > Sh_memory.max_address ){
+            //boundary check와 16진수가 아닌 경
             printf("Address Error!\nStart_address exceeds max_address\n");
             return;
         }
     }
 
-    else if( len > 4){
+    else if( len > 4){ // error
         printf("Address Error!\nStart_address exceeds max_address\n");
         return;
     }
@@ -378,28 +419,29 @@ void command_dump(){
     print_memory ( start_address, end_address );
 }
 
+/* edit 명령어 처리해주는 함수
+ * 정상적인 경우 return 1
+ * error check에 걸린 경우 return -1
+ */
 int command_edit(char *buffer){
     int address, value;
-    int len = 0, num = 0, flag = 1;
-    char *Error1 ='\0', *Error2 = '\0';
-    char *token;
+    int num = 0;
 	char tmp[256];
-    char sep[] = " \t";
 	strncpy(tmp, buffer, sizeof(tmp));
 
-	for ( int i = 0; i < strlen(tmp); ++i)
+	for ( int i = 0; i < (int)strlen(tmp); ++i)
         if ( tmp[i] == ',')
-            num++;
-    if(num != 1)
+            num++; // ','의 개수를 샌다.
+    if(num != 1) // 1개가 아닌 경우 에러
         return -1;
 
-    str_replace(tmp, ",",  " , ");
-    if( get_values(tmp) == -1)
+    str_replace(tmp, ",",  " , "); // ','를 ' , '로 다 바꿔준다.
+    if( get_values(tmp) == -1) // values에 에러 있는지 확인
         return -1;
 
     address = values[0]; value = values[1];
     
-    if( 0 <= address && address <= Sh_memory.max_address){
+    if( 0 <= address && address <= Sh_memory.max_address){ // boundary check
         Sh_memory.memory[address] = value;
         print_memory(address, address);
         return 1;
@@ -407,30 +449,31 @@ int command_edit(char *buffer){
     return -1;
 }
 
+/* fill 명령어 처리해주는 함수 
+ * 정상적인 경우 return 1
+ * error check에 걸린 경우 return -1
+ */
 int command_fill(char *buffer){
-    int num = 0, flag = 1;
-    int arr[5];
+    int num = 0;
     int start, end, value;
-    char *token, *error;
-    char sep[] = " \t";
 	char tmp[256];
 
-	strncpy(tmp, buffer, sizeof(tmp));
+	strncpy(tmp, buffer, sizeof(tmp)); // tmp에 buffer string copy
 
-    for( int i = 0; i < strlen(buffer); ++i)
-        if( buffer[i] == ',')
+    for( int i = 0; i < (int)strlen(buffer); ++i)
+        if( buffer[i] == ',') // ',' 개수 확인
             num++;
-    if(num != 2)
+    if(num != 2) // ','가 2개가 아니면 에러
         return -1;
 
-	str_replace(tmp, ",", " , ");
+	str_replace(tmp, ",", " , "); // ',' 를 ' , '로 치환해준다.
 
-    if ( get_values(tmp) == - 1)
+    if ( get_values(tmp) == - 1) // value에 에러 있는지 확인
         return -1;
 
     start = values[0], end = values[1], value = values[2];
 
-    if(start >= 0 && start <= end && end <= Sh_memory.max_address){
+    if(start >= 0 && start <= end && end <= Sh_memory.max_address){ // boundary check
         for(int i = start; i <= end; ++i)
             Sh_memory.memory[i] = value;
         print_memory(start, end);
@@ -439,14 +482,19 @@ int command_fill(char *buffer){
     return -1;
 }
 
+/* reset 명령어를 처리해주는 함수
+ * 메모리를 모두 0x00으로 초기화해준다.
+ */
 void command_reset(){
     char *memory = Sh_memory.memory;
     int i;
     for ( i = 0; i < Sh_memory.max_address; ++i )
         memory[i] = 0;
-    print_memory(0, 160);
 }
 
+/* quit 명령어를 처리해주는 함수 
+ * 추가로 동적할당한 부분을 free 해주고 종료한다.
+ */
 void process_quit(){
     printf("hihi\n");
     Lnode lptr;
@@ -467,32 +515,36 @@ void process_quit(){
     }
     exit(0);
 }
-/*
- *할당된 메모리 공간을 모두 해제해준다.
- */
 
+/* opcode 명령어를 처리해주는 함수
+ * 
+ */
 void command_opcode( ){
 
 }
 
+/* 입력한 명령어가 존재하는지 확인하는 함수
+ * 입력한 명령어가 존재하는 경우 enum Command에 해당하는 값에 맞게 출력해준다.
+ * 에러인 경우만 return -1
+ */
 int command_check(char *user_str){
     char sep[] = " \t";
     char *token;
     int i = 0, command_num = -1, len = 0;
     token = strtok(user_str, sep);
 
-    if(token == NULL)
+    if(token == NULL) // 입력한 것이 없는 경우
         return -1;
 
-    command_num = command_find(token);
-    if(command_num == -1)
+    command_num = command_find(token); // 명령어가 존재하는지 확인
+    if(command_num == -1) // 없는 경우
         return -1;
     
     while ( token != NULL ){
-        if(i > 6)
+        if(i > 6) // 이상한 명령어 입력해준경우 체크
             return -1;
         len++;
-        instruction[i++] = token;
+        instruction[i++] = token; // 명령어 저장
         token = strtok(NULL, sep);
     }
     instruction[i] = NULL;
@@ -500,25 +552,24 @@ int command_check(char *user_str){
     if ( ( 0 <= command_num && command_num <= 7 ) || command_num ==  14 
             || command_num == 16 )
         if ( len > 1 )
-            return -1;
+            return -1; // dir나 help 같은 추가로 입력할 것이 없는 명령어 에러 확인
     
     if( 0 <= command_num && command_num <= 13)
-        return command_num / 2;
+        return command_num / 2; // du[mp] 같이 입력할 수 있는 경우
     else if ( command_num > 13)
-        return command_num - 6;
-    
-    return -1;
+        return command_num - 6; // reset 같이 하나인 경우.
+    return -1; // error인 경우
 }
 
+/* 사용자가 입력한 명령어를 처리해주는 함수
+ * 명령어에 따라 적절하게 처리해준다.
+ */
 void main_process(char *buffer){
-    enum COMMAND command;
-    int command_num, Error_code;
-    char *mnemonic;
-
+    int command_num;
     strncpy( str_copy, buffer, sizeof(str_copy));
-    command_num = command_check(str_copy);
+    command_num = command_check(str_copy); // 명령어 존재하는지 확인
 
-    printf("command_num is : %d\n", command_num);
+    //printf("command_num is : %d\n", command_num);
     if(command_num != -1){
         switch(command_num){
             case help:
@@ -541,15 +592,14 @@ void main_process(char *buffer){
                 command_dump();
                 break;
 
-
             case edit:
                 strncpy( str_copy, buffer, sizeof(str_copy));
-                Error_code = command_edit(str_copy);
+                command_edit(str_copy);
                 break;
 
             case Fill:
                 strncpy( str_copy, buffer, sizeof(str_copy));
-                Error_code = command_fill(str_copy);
+                command_fill(str_copy);
                 break;
 
             case Reset:
